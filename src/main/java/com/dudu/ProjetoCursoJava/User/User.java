7f50df058da3c140b00d921182c5a9d430182b24
Eduardo Henrique <eduardo.h.anjos0411@gmail.com>
2025-02-10 23:13:28 -0300

    add h2 database, create test profile, configure JPA

--- src/main/java/com/dudu/ProjetoCursoJava/User/User.java
+++ src/main/java/com/dudu/ProjetoCursoJava/User/User.java
@@ -2,13 +2,22 @@ package com.dudu.ProjetoCursoJava.User;
 
 import java.io.Serializable;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter @Setter @EqualsAndHashCode 
+@Entity
+@Table(name="tb_user")
 public class User implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String name;
     private String email;
---
+++ src/main/java/com/dudu/ProjetoCursoJava/User/userRepository.java
@@ -0,0 +1,7 @@
+package com.dudu.ProjetoCursoJava.User;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface userRepository extends JpaRepository<User,Long>{
+    
+}
