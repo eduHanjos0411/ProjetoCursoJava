2f7adc6b2a7363846562aa55a6eb731634b0e7c1
Eduardo Henrique <eduardo.h.anjos0411@gmail.com>
2025-02-10 22:38:05 -0300

    Initial commit

---
+++ src/main/java/com/dudu/ProjetoCursoJava/ProjetoCursoJavaApplication.java
@@ -0,0 +1,13 @@
+package com.dudu.ProjetoCursoJava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoCursoJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoCursoJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dudu/ProjetoCursoJava/ProjetoCursoJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dudu.ProjetoCursoJava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoCursoJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
